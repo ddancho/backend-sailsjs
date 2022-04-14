574f7e53b35b158b13565cfeb6f4eae2977ec1a4
Dancho <ddancho@gmail.com>
2022-04-14 09:21:16 +0200

    created category seeder

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,8 +9,7 @@
  * https://sailsjs.com/config/bootstrap
  */
 
-module.exports.bootstrap = async function() {
-
+module.exports.bootstrap = async function () {
   // By convention, this is a good place to set up fake data during development.
   //
   // For example:
@@ -27,4 +26,20 @@ module.exports.bootstrap = async function() {
   // ]);
   // ```
 
+  // seed categories table with default values for movie category
+  // check if there are any records in the table
+  if ((await Category.count()) > 0) {
+    return;
+  }
+
+  // there is not, seed data
+  // thriller, horror, drama, fantasy, comedy, action
+  await Category.createEach([
+    { title: 'thriller', slug: await sails.helpers.createSlug('thriller') },
+    { title: 'horror', slug: await sails.helpers.createSlug('horror') },
+    { title: 'drama', slug: await sails.helpers.createSlug('drama') },
+    { title: 'fantasy', slug: await sails.helpers.createSlug('fantasy') },
+    { title: 'comedy', slug: await sails.helpers.createSlug('comedy') },
+    { title: 'action', slug: await sails.helpers.createSlug('action') },
+  ]);
 };
